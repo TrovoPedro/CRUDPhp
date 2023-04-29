66384c17b1e9fbc6f69dcfd85c001d75fd9632e2
Pedro Trovo <98984115+TrovoPedro@users.noreply.github.com>
2023-04-29 03:36:05 -0300

    Add files via upload

---
+++ cadastrar.php
@@ -0,0 +1,14 @@
+<h1>Cadastro de usuarios</h1>
+
+<form method="POST" action="cadastrar_action.php">
+    <label>
+        Nome: <input type="text" name="nome">
+    </label>
+    <label>
+        Email: <input type="text" name="email">
+    </label>
+    <label>
+        Senha: <input type="password" name="senha">
+    </label>
+    <input type="submit" value="Cadastrar">
+</form>
---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+require './configs/config.php';
+
+?>
+
+<h1>Listagem de Usuarios </h1>
+
+<table border="1">
+    <tr>
+        <th>ID</th>
+        <th>Nome</th>
+        <th>Email</th>
+        <th>Senha</th>
+        <th>Ações</th>
+    </tr>
+</table>
+
+<a href="cadastrar.php">Cadastrar usuarios</a>
